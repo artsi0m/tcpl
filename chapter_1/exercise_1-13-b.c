ddb58f76bc2900b87bc5ca10eb3daaf20c8d06d2
artsi0m <aning@protonmail.ch>
2020-10-07 01:25:11 +0300

    Fixed exercise 1-13-b, now it prints horizontal hystogram

--- chapter_1/exercise_1-13-b.c
+++ chapter_1/exercise_1-13-b.c
@@ -1,24 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* values of state variable */
+/* Values of state variable */
 #define WORD 0
 #define FIRST_WHITESPACE 1
+#define MAX_ELEMENTS 25
 
-int main(){
+int main()
+{
     int c;
     int *nc;
-    int **matrix;
 
-    size_t max_number_of_words = 100;
-    int nw = 0;
     int state = FIRST_WHITESPACE;
-
+    int nw = 0;
+    size_t max_number_of_words = MAX_ELEMENTS;
     nc = calloc(max_number_of_words, sizeof(int));
-    matrix = calloc(max_number_of_words, sizeof(int *));
-    for (int i = 0; i < (int) max_number_of_words; ++i) {
-        matrix[i] = calloc(max_number_of_words, sizeof(int));
-    }
 
     while ((c = getchar()) != EOF) {
         if (c != ' ' && c != '\t' && c != '\n') {
@@ -36,27 +32,14 @@ int main(){
         }
     }
 
-    /* Filling matrix */
-    for (int a = 0; a < (int) max_number_of_words; ++a) {
-        for (int b = 0; b < nc[a]; ++b) {
-            matrix[b][a] = '#';
+    for (int i = MAX_ELEMENTS; i > 0; --i) {
+        for (int j = 0; j < MAX_ELEMENTS; ++j) {
+            if (i <= nc[j]) {
+                putchar('#');
+            } else {
+                putchar(' ');
+            }
         }
-        for (int b = nc[a]; b < (int) max_number_of_words - 1; ++b) {
-            matrix[b][a] = ' ';
-        }
-        matrix[(int) max_number_of_words][a] = '\n';
+        putchar('\n');
     }
-    /* Printing matrix */
-    for (int a = 0; a < (int) max_number_of_words; ++a) {
-        for (int b = 0; b < (int) max_number_of_words; ++b) {
-            putchar(matrix[a][b]);
-        }
-    }
-
-    free(nc);
-    for (int i = 0; i < (int) max_number_of_words; ++i) {
-        free(*matrix);
-    }
-    free(matrix);
-    return 0;
 }
