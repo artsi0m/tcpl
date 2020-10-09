9268fd548644fe035d631e1e95c3df26b55eca11
artsi0m <aning@protonmail.ch>
2020-10-09 03:31:19 +0300

    Now exercise 1-14 prints hystogram of charcters

--- chapter_1/exercise_1-14.c
+++ chapter_1/exercise_1-14.c
@@ -1,24 +1,43 @@
 #include <stdio.h>
 
+void put_shebang_line_for_each_char(int c)
+{
+    putchar(' ');
+    for (int i = 0; i < c; ++i) {
+        putchar('#');
+    }
+    putchar('\n');
+}
+
 int main()
 {
     int c;
     int ch[128];
+
     for (int i = 0; i < 128; ++i) {
         ch[i] = 0;
     }
+
     while ((c = getchar()) != EOF) {
         ++ch[c];
     }
+
     for (int i = 0; i < 128; ++i) {
         if (i == '\n') {
-            printf("\\n %d \n", ch[i]);
+            putchar('\\');
+            putchar('n');
+            put_shebang_line_for_each_char(ch[i]);
         } else if (i == '\t') {
-            printf("\\t %d \n", ch[i]);
+            putchar('\\');
+            putchar('t');
+            put_shebang_line_for_each_char(ch[i]);
         } else if (i == '\b') {
-            printf("\\b %d \n", ch[i]);
+            putchar('\\');
+            putchar('b');
+            put_shebang_line_for_each_char(ch[i]);
         } else {
-            printf("%c %d \n", i, ch[i]);
+            putchar(i);
+            put_shebang_line_for_each_char(ch[i]);
         }
     }
     return 0;
