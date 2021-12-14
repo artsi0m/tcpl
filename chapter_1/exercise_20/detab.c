102d634ee91caa0f5e7956659c250072f1a7281b
Artsi0m <aning@protonmail.ch>
2021-12-14 22:36:56 +0300

    Exercise 20 done

---
+++ chapter_1/exercise_20/detab.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define TABSTOP 8
+
+int
+main(void)
+{
+	int c;
+	while ((c = getchar()) != EOF){
+		if (c == '\t'){
+			for(int i=0; i < TABSTOP; i++)
+				putchar(' ');
+		} else { putchar(c); }
+	}
+}
