3d628aa9f92fa9d0faac813912458aa39c3e8f29
Artsi0m <aning@protonmail.ch>
2022-08-22 20:23:45 +0300

    Worked on case when there is no spaces in given lines

--- chapter_1/exercise_22/fold.c
+++ chapter_1/exercise_22/fold.c
@@ -7,17 +7,17 @@
 #define MAXLINE 1000
 #define FOLDLINE 20
 
-void	fold(char *, char *, int );
+void	fold(char *, char *, int);
 
 int
 main(void)
 {
 	int len;		/* current line length */
-	char line[MAXLINE];	
+	char line[MAXLINE];
 	char folded_line[MAXLINE];
 
-	while ((len = getchararr(line, MAXLINE)) > 0 ) {
-		if (len > FOLDLINE){
+	while ((len = getchararr(line, MAXLINE)) > 0) {
+		if (len > FOLDLINE) {
 			fold(folded_line, line, FOLDLINE);
 			fputs(folded_line, stdout);
 		} else {
@@ -33,32 +33,33 @@ fold(char ret[], char s[], int fold_lim)
 	/*
 	 * i -- counter for position in s[]
 	 * j -- counter for unfolded part of ret[]
+	 * k -- counter for position in s[] in second pass
+	 * l -- number of folds in ret[] in a second pass
 	 */
-	int i, j, k;
+	int i, j, k, l;
 	int space_position = 0;
 
-	for (i = 0, j = 0; i < MAXLINE - 1; ++i, ++j){
+	for (i = 0, j = 0; i < MAXLINE - 1; ++i, ++j) {
 		ret[i] = s[i];
-		
-		if((s[i] == ' ' || s[i] == '\t') && j < fold_lim)
+
+		if ((s[i] == ' ' || s[i] == '\t') && j < fold_lim)
 			space_position = i;
-		
-		if(j > fold_lim && space_position != 0){
+
+		if (j > fold_lim && space_position != 0) {
 			ret[space_position] = '\n';
 			j = 0;
-		} 
-
+		}
 	}
 
-	if(j > fold_lim && space_position == 0){
-		for(i = 0, k = 0; i < MAXLINE - 1; ++i, ++k){
+	if (j > fold_lim) {
+		for (i = 0, k = 0; i < MAXLINE - 1; ++i, ++k) {
 			ret[i] = s[k];
-			
-			if(i == fold_lim){
-				ret[fold_lim] = '\n';
+
+			if (i % fold_lim == 0) {
+				l = i / fold_lim;
+				ret[fold_lim * l] = '\n';
 				k--;
 			}
 		}
 	}
-
 }
